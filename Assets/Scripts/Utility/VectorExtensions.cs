9116517b3e75ef61dc52d96dd13edc02b2a49ab0
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-03-09 22:37:09 +0100

    Improved perpendicular vector utility methods

--- Assets/Scripts/Utility/VectorExtensions.cs
+++ Assets/Scripts/Utility/VectorExtensions.cs
@@ -5,28 +5,25 @@ namespace Utility
 {
     public static class VectorExtensions
     {
-        public static Vector2 Perpendicular(this Vector2 vector)
+        public static Vector2 GetPerpendicularVector(this Vector2 vector)
         {
             if (vector == Vector2.zero)
             {
-                throw new ArgumentException(
-                    "Can't find perpendicular vector of a zero vector.", nameof(vector));
+                throw new ArgumentException($"{nameof(vector)} must be a non zero vector.");
             }
-            
-            return new Vector2(-vector.y, vector.x);
-        }
 
-        public static Vector3 Perpendicular(this Vector3 vector)
+            return new Vector2(-vector.y, vector.x).normalized;
+        }
+        
+        public static Vector3 GetPerpendicularVector(this Vector3 vector, Vector3 planeNormal)
         {
-            if (vector == Vector3.zero)
+            if (vector == Vector3.zero || planeNormal == Vector3.zero)
             {
                 throw new ArgumentException(
-                    "Can't find perpendicular vector of a zero vector.", nameof(vector));
+                    $"{nameof(vector)} and {nameof(planeNormal)} must be non zero vectors.");
             }
 
-            return vector is { x: 0f, y: 0f } 
-                ? new Vector3(vector.z, 0f, 0f) 
-                : new Vector3(-vector.y, vector.x, 0f);
+            return Vector3.Cross(planeNormal, vector).normalized;
         }
     }
 }
