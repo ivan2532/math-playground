70b4bf766d10b5b292be61658a400886e95877fe
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-03-09 18:43:20 +0100

    Utility methods for finding perpendicular vectors

---
+++ Assets/Scripts/Utility/VectorExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 Perpendicular(this Vector2 vector)
+        {
+            if (vector == Vector2.zero)
+            {
+                throw new ArgumentException(
+                    "Can't find perpendicular vector of a zero vector.", nameof(vector));
+            }
+            
+            return new Vector2(-vector.y, vector.x);
+        }
+
+        public static Vector3 Perpendicular(this Vector3 vector)
+        {
+            if (vector == Vector3.zero)
+            {
+                throw new ArgumentException(
+                    "Can't find perpendicular vector of a zero vector.", nameof(vector));
+            }
+
+            return vector is { x: 0f, y: 0f } 
+                ? new Vector3(vector.z, 0f, 0f) 
+                : new Vector3(-vector.y, vector.x, 0f);
+        }
+    }
+}
